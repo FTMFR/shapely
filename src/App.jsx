e16aa83db59d6fbdede4659910e059a09dc8c554
FTMFR <fatemefarajzade018@gmail.com>
2023-12-29 18:30:37 +0330

    add options component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/header/Header";
 import EffectPart from "./components/effectPart/EffectPart";
 import Content from "./components/content/Content";
 import ReverseContent from "./components/reverseContent/ReverseContent";
+import Options from "./components/options/Options";
 import "./App.css";
 
 function App() {
@@ -11,6 +12,8 @@ function App() {
       <EffectPart />
       <Content />
       <ReverseContent />
+      <EffectPart />
+      <Options />
     </>
   );
 }
---
+++ src/components/options/Options.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./options.scss";
+
+const Options = () => {
+  return (
+    <div className="container">
+      <div className="contentBox">
+        <p>Limitless Options</p>
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+          doloribus voluptatem similique modi repellat quia quisquam in quaerat
+          doloremque enim!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+          doloribus voluptatem similique modi repellat quia quisquam in quaerat
+        </span>
+        <button>DOWNLOAD NOW</button>
+        <img src="./assets/options.jpg" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Options;
